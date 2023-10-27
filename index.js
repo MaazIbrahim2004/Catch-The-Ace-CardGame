ceacaf9dd0d3d7bc8c296618b20fc9f643bd86eb
Maaz Ibrahim <maazibrahim2004@gmail.com>
2023-10-26 23:55:54 -0400

    updated js
    added card elements to store cards

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ const cardObjectDefenitions = [
     {id:4,imagePath: '/images/aceOfSpades.png'},
 ]
 
+const cardBackImgPath = '/images/card-back-blue.png'
+
+const cardContainerElem = document.querySelector('.card-container')
+
 function createCard(cardItem) {
     // creating div elements that make up the card
     const cardElem = createElement('div')
@@ -25,6 +29,40 @@ function createCard(cardItem) {
 
     // add class to the inner card element
     addClassToElement(cardInnerElem, 'card-inner')
+
+    // add class to front card element
+    addClassToElement(cardFrontElem, 'card-front')
+
+    // add class to back card element
+    addClassToElement(cardBackElem, 'card-back')
+
+    // add src attribute and appropriate value to img element - back of card
+    addSrcToImageElem(cardBackImg, cardBackImgPath)
+
+    // add src attribute and appropriate value to img element - front of card
+    addSrcToImageElem(cardFrontImg, cardItem.imagePath)
+
+    // assign class to back image element of back of card
+    addClassToElement(cardBackImg, 'card-img')
+
+    // assign class to front image element of front of card
+    addClassToElement(cardFrontImg, 'card-img')
+
+    // add front image element as child element to front card element
+    addChildElement(cardFrontElem, cardFrontImg)
+
+    // add back image element as child element to back card element
+    addChildElement(cardBackElem, cardBackImg)
+
+    // add front card element as child element to inner card element
+    addChildElement(cardInnerElem, cardBackElem)
+
+    // add inner card element as child element to card element
+    addChildElement(cardElem, cardInnerElem)
+
+
+
+
 }
 
 function createElement(elemType) {
@@ -37,4 +75,12 @@ function addClassToElement(elem, className) {
 
 function addIdToElement(elem, id) {
     elem.id = id
+}
+
+function addSrcToImageElem(imgElem, src) {
+    imgElem.src = src
+}
+
+function addChildElement( parentElem, childElem) {
+    parentElem.appendChild(childElem)
 }
