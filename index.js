48feb1cbee8f2adcb85cf7d3dc5c63e540eb5668
Maaz Ibrahim <maazibrahim2004@gmail.com>
2023-11-04 23:50:29 -0400

    updated js
    added new round and start round functionality. Added effects for cards, for the cards to flip and also fly in when pressing button

--- index.js
+++ index.js
@@ -32,16 +32,38 @@ function startGame() {
 }
 
 function initializeNewGame() {
+    score = 0
+    roundNum = 0
 
+    checkForIncompleteGame()
+
+    shufflingInProgress = false
+
+    updateStatusElement(scoreContainerElem,"flex")
+    updateStatusElement(roundContainerElem,"flex")
+
+    updateStatusElement(scoreElem,"block",primaryColor,`Score <span class='badge'>${score}</span>`)
+    updateStatusElement(roundElem,"block",primaryColor,`Round <span class='badge'>${roundNum}</span>`)
 }
 
 function startRound() {
     initializeNewRound()
     collectCards()
+    flipCards(true)
+    shuffleCards()
 }
 
 function initializeNewRound() {
+    roundNum++
+    playGameButtonElem.disabled = true
 
+    gameInProgress = true
+    shufflingInProgress = true
+    cardsRevealed = false
+
+    updateStatusElement(currentGameStatusElem, "block", primaryColor, "Shuffling...")
+
+    updateStatusElement(roundElem, "block", primaryColor, `Round <span class='badge'>${roundNum}</span>`)
 }
 
 function collectCards() {
@@ -59,6 +81,53 @@ function addCarddstoGridAreaCell(cellPositionClassName) {
     card.forEach((card, index) =>{
         addChildElement(cellPositionElem, card)
     })
+}
+
+function flipCard(card, flipToBack)
+{
+    const innerCardElem = card.firstChild
+
+    if(flipToBack && !innerCardElem.classList.contains('flip-it'))
+    {
+        innerCardElem.classList.add('flip-it')
+    }
+    else if(innerCardElem.classList.contains('flip-it'))
+    {
+        innerCardElem.classList.remove('flip-it')
+    }
+
+}
+
+function flipCards(flipToBack){
+    cards.forEach((card,index)=>{
+        setTimeout(() => {
+            flipCard(card,flipToBack)
+        },index * 100)
+    })
+}
+
+function cardFlyInEffect()
+{
+    const id = setInterval(flyIn, 5)
+    let cardCount = 0
+
+    let count = 0
+
+    function flyIn()
+    {
+        count++
+        if(cardCount == numCards)
+        {
+            clearInterval(id)
+            playGameButtonElem.style.display = "inline-block"
+        }
+        if(count == 1 || count == 250 || count == 500 || count == 750)
+        {
+            cardCount++
+            let card = document.getElementById(cardCount)
+            card.classList.remove("fly-in")
+        }
+    }
 }
 
 function createCards()
