e1fc63b94f03dd6c470f7572004bd073c1837566
Maaz Ibrahim <maazibrahim2004@gmail.com>
2023-11-10 23:10:26 -0500

    updated css and js
    Added all animations in css. Added score, round, and game functionality, completing the game

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-// array of card object defenitions, each with an id and an image path
+// array of card object definitions, each with an id and an image path
 const cardObjectDefinitions = [
     {id:1, imagePath:'./images/card-KingHearts.png'},
     {id:2, imagePath:'./images/card-JackClubs.png'},
@@ -7,31 +7,232 @@ const cardObjectDefinitions = [
     {id:4, imagePath:'./images/card-AceSpades.png'}
 ]
 
-const cardBackImgPath = './images/card-back-blue.png'
+const aceId = 4
+
+const cardBackImgPath = '/images/card-back-blue.png'
 
 let cards = []
 
-const plaayGameBUttonElem = document.getElementById('playGame')
+const playGameButtonElem = document.getElementById('playGame')
 
 const cardContainerElem = document.querySelector('.card-container')
 
-const collapsedGridAreaTemplate = '"a a "a a"'
+const collapsedGridAreaTemplate = '"a a" "a a"'
 const cardCollectionCellClass = ".card-pos-a"
 
-function loadGame() {
+const numCards = cardObjectDefinitions.length
+
+let cardPositions = []
+
+
+let gameInProgress = false
+let shufflingInProgress = false
+let cardsRevealed = false
+
+
+const currentGameStatusElem = document.querySelector('.current-status')
+const scoreContainerElem = document.querySelector('.header-score-container')
+const scoreElem = document.querySelector('.score')
+const roundContainerElem = document.querySelector('.header-round-container')
+const roundElem = document.querySelector('.round')
+
+const winColor = "green"
+const loseColor = "red"
+const primaryColor = "black"
+
+let roundNum = 0
+let maxRounds = 4
+let score = 0
+
+let gameObj = {}
+
+const localStorageGameKey = "HTA"
+
+
+/* <div class="card">
+<div class="card-inner">
+    <div class="card-front">
+        <img src="/images/card-JackClubs.png" alt="" class="card-img">
+    </div>
+    <div class="card-back">
+        <img src="/images/card-back-Blue.png" alt="" class="card-img">
+    </div>
+</div>
+</div> */
+
+
+loadGame()
+
+
+function gameOver()
+{
+    updateStatusElement(scoreContainerElem,"none")
+    updateStatusElement(roundContainerElem,"none")
+
+    const gameOverMessage = `Game Over! Final Score - <span class = 'badge'>${score}</span> Click 'Play Game' button to play again`
+
+    updateStatusElement(currentGameStatusElem,"block",primaryColor,gameOverMessage)
+
+    gameInProgress = false
+    playGameButtonElem.disabled = false
+}
+
+function endRound()
+{
+    setTimeout(()=>{
+        if(roundNum == maxRounds)
+        {
+            gameOver()
+            return
+        }
+        else
+        {
+            startRound()
+        }
+    },3000)
+}
+
+function chooseCard(card)
+{
+    if(canChooseCard())
+    {
+        evaluateCardChoice(card)
+        saveGameObjectToLocalStorage(score, roundNum)
+        flipCard(card,false)
+
+        setTimeout(() => {
+            flipCards(false)
+            updateStatusElement(currentGameStatusElem,"block", primaryColor,"Card positions revealed")
+
+            endRound()
+
+        },3000)
+        cardsRevealed = true
+    }
+
+}
+
+function calculateScoreToAdd(roundNum)
+{
+    if(roundNum == 1)
+    {
+        return 100
+    }
+    else if(roundNum == 2)
+    {
+        return 50
+    }
+    else if(roundNum == 3)
+    {
+        return 25
+    }
+    else
+    {
+        return 10
+    }
+}
+
+function calculateScore()
+{
+    const scoreToAdd = calculateScoreToAdd(roundNum)
+    score = score + scoreToAdd
+}
+
+function updateScore()
+{
+    calculateScore()
+    updateStatusElement(scoreElem, "block", primaryColor, `Score <span class='badge'>${score}</span>`)
+
+}
+
+function updateStatusElement(elem, display, color, innerHTML)
+{
+    elem.style.display = display
+
+    if(arguments.length > 2)
+    {
+        elem.style.color = color
+        elem.innerHTML = innerHTML
+    }
+
+}
+
+function outputChoiceFeedBack(hit)
+{
+    if(hit)
+    {
+        updateStatusElement(currentGameStatusElem, "block", winColor, "Hit!! - Well Done!! :)")
+    }
+    else
+    {
+        updateStatusElement(currentGameStatusElem, "block", loseColor, "Missed!! :(")
+    }
+}
+
+function evaluateCardChoice(card)
+{
+    if(card.id == aceId)
+    {
+        updateScore()
+        outputChoiceFeedBack(true)
+    }
+    else
+    {
+        outputChoiceFeedBack(false)
+    }
+}
+
+function canChooseCard()
+{
+    return gameInProgress == true && !shufflingInProgress && !cardsRevealed
+}
+
+
+
+function loadGame(){
     createCards()
 
     cards = document.querySelectorAll('.card')
 
+    cardFlyInEffect()
+
     playGameButtonElem.addEventListener('click', ()=>startGame())
 
-}
-function startGame() {
-    initializeNewGame()
-    startRound()
+    updateStatusElement(scoreContainerElem,"none")
+    updateStatusElement(roundContainerElem,"none")
+
 }
 
-function initializeNewGame() {
+function checkForIncompleteGame()
+{
+    const serializedGameObj = getLocalStorageItemValue(localStorageGameKey)
+    if(serializedGameObj)
+    {
+        gameObj = getObjectFromJSON(serializedGameObj)
+
+        if(gameObj.round >= maxRounds)
+        {
+            removeLocalStorageItem(localStorageGameKey)
+        }
+        else
+        {
+            if(confirm('Would you like to continue with your last game?'))
+            {
+                score = gameObj.score
+                roundNum = gameObj.round
+            }
+        }
+
+    }
+
+}
+
+function startGame(){
+    initializeNewGame()
+    startRound()
+
+}
+function initializeNewGame(){
     score = 0
     roundNum = 0
 
@@ -44,16 +245,18 @@ function initializeNewGame() {
 
     updateStatusElement(scoreElem,"block",primaryColor,`Score <span class='badge'>${score}</span>`)
     updateStatusElement(roundElem,"block",primaryColor,`Round <span class='badge'>${roundNum}</span>`)
-}
 
-function startRound() {
+}
+function startRound()
+{
     initializeNewRound()
     collectCards()
     flipCards(true)
     shuffleCards()
-}
 
-function initializeNewRound() {
+}
+function initializeNewRound()
+{
     roundNum++
     playGameButtonElem.disabled = true
 
@@ -64,23 +267,28 @@ function initializeNewRound() {
     updateStatusElement(currentGameStatusElem, "block", primaryColor, "Shuffling...")
 
     updateStatusElement(roundElem, "block", primaryColor, `Round <span class='badge'>${roundNum}</span>`)
+
 }
 
-function collectCards() {
+function collectCards(){
     transformGridArea(collapsedGridAreaTemplate)
-    addCarddstoGridAreaCell(cardCollectionCellClass)
+    addCardsToGridAreaCell(cardCollectionCellClass)
+
 }
 
-function transformGridArea(areas) {
+function transformGridArea(areas)
+{
     cardContainerElem.style.gridTemplateAreas = areas
-}
 
-function addCarddstoGridAreaCell(cellPositionClassName) {
+}
+function addCardsToGridAreaCell(cellPositionClassName)
+{
     const cellPositionElem = document.querySelector(cellPositionClassName)
 
-    card.forEach((card, index) =>{
+    cards.forEach((card, index) =>{
         addChildElement(cellPositionElem, card)
     })
+
 }
 
 function flipCard(card, flipToBack)
@@ -128,7 +336,135 @@ function cardFlyInEffect()
             card.classList.remove("fly-in")
         }
     }
+
+
+
 }
+
+function removeShuffleClasses()
+{
+    cards.forEach((card) =>{
+        card.classList.remove("shuffle-left")
+        card.classList.remove("shuffle-right")
+    })
+}
+function animateShuffle(shuffleCount)
+{
+    const random1 = Math.floor(Math.random() * numCards) + 1
+    const random2 = Math.floor(Math.random() * numCards) + 1
+
+    let card1 = document.getElementById(random1)
+    let card2 = document.getElementById(random2)
+
+    if (shuffleCount % 4 == 0)
+    {
+        card1.classList.toggle("shuffle-left")
+        card1.style.zIndex = 100
+    }
+    if (shuffleCount % 10 == 0)
+    {
+        card2.classList.toggle("shuffle-right")
+        card2.style.zIndex = 200
+    }
+
+}
+
+function shuffleCards()
+{
+    let shuffleCount = 0
+    const id = setInterval(shuffle, 12)
+
+
+    function shuffle()
+    {
+        randomizeCardPositions()
+
+        animateShuffle(shuffleCount)
+
+        if(shuffleCount == 500)
+        {
+            clearInterval(id)
+            shufflingInProgress = false
+            removeShuffleClasses()
+            dealCards()
+            updateStatusElement(currentGameStatusElem, "block", primaryColor, "Please click the card that you think is the Ace of Spades...")
+
+        }
+        else{
+            shuffleCount++
+        }
+
+    }
+
+}
+function randomizeCardPositions()
+{
+    const random1 = Math.floor(Math.random() * numCards) + 1
+    const random2 = Math.floor(Math.random() * numCards) + 1
+
+    const temp = cardPositions[random1 - 1]
+
+    cardPositions[random1 - 1] = cardPositions[random2 - 1]
+    cardPositions[random2 - 1] = temp
+
+}
+function dealCards()
+{
+    addCardsToAppropriateCell()
+    const areasTemplate = returnGridAreasMappedToCardPos()
+
+    transformGridArea(areasTemplate)
+
+}
+function returnGridAreasMappedToCardPos()
+{
+    let firstPart = ""
+    let secondPart = ""
+    let areas = ""
+
+    cards.forEach((card, index) => {
+        if(cardPositions[index] == 1)
+        {
+            areas = areas + "a "
+        }
+        else if(cardPositions[index] == 2)
+        {
+            areas = areas + "b "
+        }
+        else if (cardPositions[index] == 3)
+        {
+            areas = areas + "c "
+        }
+        else if (cardPositions[index] == 4)
+        {
+            areas = areas + "d "
+        }
+        if (index == 1)
+        {
+            firstPart = areas.substring(0, areas.length - 1)
+            areas = "";
+        }
+        else if (index == 3)
+        {
+            secondPart = areas.substring(0, areas.length - 1)
+        }
+
+    })
+
+    return `"${firstPart}" "${secondPart}"`
+
+
+}
+
+
+function addCardsToAppropriateCell()
+{
+    cards.forEach((card)=>{
+        addCardToGridCell(card)
+    })
+}
+
+
 
 function createCards()
 {
@@ -136,6 +472,7 @@ function createCards()
         createCard(cardItem)
     })
 }
+
 
 function createCard(cardItem){
 
@@ -192,6 +529,20 @@ function createCard(cardItem){
 
     //add card element as child element to appropriate grid cell
     addCardToGridCell(cardElem)
+
+    initializeCardPositions(cardElem)
+
+    attatchClickEventHandlerToCard(cardElem)
+
+
+}
+function attatchClickEventHandlerToCard(card){
+    card.addEventListener('click', () => chooseCard(card))
+}
+
+function initializeCardPositions(card)
+{
+    cardPositions.push(card.id)
 }
 
 function createElement(elemType){
@@ -238,4 +589,37 @@ function mapCardIdToGridCell(card){
     {
         return '.card-pos-d'
     }
+}
+
+//local storage functions
+function getSerializedObjectAsJSON(obj)
+{
+    return JSON.stringify(obj)
+}
+function getObjectFromJSON(json)
+{
+    return JSON.parse(json)
+}
+function updateLocalStorageItem(key, value)
+{
+    localStorage.setItem(key, value)
+}
+function removeLocalStorageItem(key)
+{
+    localStorage.removeItem(key)
+}
+function getLocalStorageItemValue(key)
+{
+    return localStorage.getItem(key)
+}
+
+function updateGameObject(score,round)
+{
+    gameObj.score = score
+    gameObj.round = round
+}
+function saveGameObjectToLocalStorage(score,round)
+{
+    updateGameObject(score, round)
+    updateLocalStorageItem(localStorageGameKey, getSerializedObjectAsJSON(gameObj))
 }
